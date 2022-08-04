c8b5fedd4189e1a761af63c67852996ccb148d3f
oat20 <iceeagle99@gmail.com>
2022-08-04 09:44:23 +0700

    Update sendmail-weekagenda-cronjob.php

--- directmail/sendmail-weekagenda-cronjob.php
+++ directmail/sendmail-weekagenda-cronjob.php
@@ -66,5 +66,52 @@ curl_close ($ch);
 }
 //line notify
 
+//linenotify studio room
+$qEvent = mysqli_query($mysqli, "SELECT *
+FROM meetingroom_userq AS t1
+INNER JOIN meetingroom_croom AS t2 ON ( t1.cr_id = t2.cr_id )
+WHERE CONCAT( t1.Dater, ' ', t1.time_in, ':00' ) >= CURRENT_TIMESTAMP( )
+AND t1.uq_cancel LIKE 'No'
+AND t2.cr_id = '5776351'
+ORDER BY t1.Dater, t1.time_in, t1.time_out
+LIMIT 1
+");
+$rowEvent = mysqli_num_rows($qEvent);
+if($rowEvent > 0){
+
+	$rs_lastbooking = mysqli_fetch_assoc($qEvent);
+	$lastbooking = date('d/m/Y', strtotime($rs_lastbooking['Dater'])).' '.$rs_lastbooking['time_in'].' - '.$rs_lastbooking['time_out'].' '.$rs_lastbooking['title'];
+	 
+	//send line notify
+	$token = "97PZQvmxrLBM7jvPlCwMZ5yq6msIpMQCMtTpljfoqIP"; //ใส่Token ที่copy เอาไว้
+	
+	$str = "STUDIO ROOM @ ".$lastbooking." ".$livesite."bookingroom/inc/roomview.inc.php?keyID=".$rs_lastbooking['uq_id'];
+	
+	$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL,"https://notify-api.line.me/api/notify");
+curl_setopt($ch, CURLOPT_POST, 1);
+// In real life you should use something like:
+curl_setopt($ch, CURLOPT_POSTFIELDS, 
+          http_build_query(
+			array(
+				'message' => $str
+			)));
+
+// Receive server response ...
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		'Content-Type: application/x-www-form-urlencoded',
+		'Authorization: Bearer '.$token
+	));
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+
+$server_output = curl_exec($ch);
+
+curl_close ($ch);	
+}
+//linenotify studio room
+
 //include('../bookingroom/js-inc.php');
 ?>
