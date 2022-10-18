5a42535469a489b49f823c9803d0ef318b55c481
oat20 <iceeagle99@gmail.com>
2022-10-18 17:19:18 +0700

    Create first-current-booking.php

---
+++ api/first-current-booking.php
@@ -0,0 +1,37 @@
+<?php
+require_once '../bookingroom/config.php';
+require_once '../bookingroom/mysqli_connect.php';
+
+if($_SERVER['REQUEST_METHOD']=='GET'){
+    $response=array(
+        'status'=>'OK',
+    );
+    $sql2=mysqli_query($mysqli,"SELECT date_format(t1.Dater, '%d/%m/%Y') as date_display,
+t1.time_in,
+t1.time_out,
+t1.title,
+concat(t2.name,' ',t2.location) as room
+FROM meetingroom_userq AS t1
+INNER JOIN meetingroom_croom AS t2 ON ( t1.cr_id = t2.cr_id )
+WHERE CONCAT( t1.Dater, ' ', t1.time_in, ':00' ) >= CURRENT_TIMESTAMP( )
+AND t1.uq_cancel LIKE 'No'
+ORDER BY t1.Dater, t1.time_in, t1.time_out, CONVERT( CONCAT( t2.name, ' ', t2.location )
+USING tis620 )
+LIMIT 1");
+$rs2=mysqli_fetch_assoc($sql2);
+$response['data'][]=array(
+    'dater'=>$rs2['date_display'],
+    'timein'=>$rs2['time_in'],
+    'timeout'=>$rs2['time_out'],
+    'room'=>$rs2['room'],
+    'title'=>$rs2['title']
+);
+http_response_code(200);
+echo json_encode($response, JSON_UNESCAPED_UNICODE);
+}else{
+    http_response_code(400);
+    echo json_encode(array(
+        'status'=>'Error'
+    ));
+}
+?>
