2c29c0d6db30f64c2e6ce43503f6797a0541fa4d
oat20 <iceeagle99@gmail.com>
2022-10-19 09:47:57 +0700

    Update first-current-booking.php

--- api/first-current-booking.php
+++ api/first-current-booking.php
@@ -26,10 +26,10 @@ $response['data'][]=array(
     'room'=>$rs2['room'],
     'title'=>$rs2['title']
 );
-http_response_code(200);
-echo json_encode($response, JSON_UNESCAPED_UNICODE);
+//http_response_code(200);
+echo json_encode($response);
 }else{
-    http_response_code(400);
+    //http_response_code(400);
     echo json_encode(array(
         'status'=>'Error'
     ));
