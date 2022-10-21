5fcfef41d906531f44e7fda59e52c9810bde63d7
oat20 <iceeagle99@gmail.com>
2022-10-21 11:08:18 +0700

    update

--- api/current-booking.php
+++ api/current-booking.php
@@ -1,4 +1,14 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+	
+	header("Content-Type: application/json; charset=UTF-8");
+	
+	header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+	
+	header("Access-Control-Max-Age: 3600");
+	
+	header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 require_once '../bookingroom/config.php';
 require_once '../bookingroom/mysqli_connect.php';
 
